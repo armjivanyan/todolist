ac96d4fa5b4537bc05e377f20dce8b6d968d00d6
armjivanyan <arm.jivanyan@gmail.com>
2023-07-07 01:56:34 +0400

    hidden keys

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTba5JrPo8pJlPCjsuWKNal05obR6FjIk",
-  authDomain: "todolist-454de.firebaseapp.com",
-  projectId: "todolist-454de",
-  storageBucket: "todolist-454de.appspot.com",
-  messagingSenderId: "142171324142",
-  appId: "1:142171324142:web:d98be9585cacdfdc253dff",
-  measurementId: "G-YHK3VCYT0E"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
